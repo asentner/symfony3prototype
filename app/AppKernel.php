e9f1c0c5df7ad9c7adcf8e1f6033f1c75306c836
Adam Sentner <asentner@coalmarch.com>
2016-07-29 21:33:48 -0400

    Adding app name, version to App Kernel. Also initializing dependency bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,21 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+    /**
+     * Kernel Name
+     */
+    const NAME = 'Prototype';
+
+    /**
+     * Version Defines
+     */
+    const VERSION = '1.0.0';
+    const VERSION_ID = '10000';
+    const MAJOR_VERSION = '1';
+    const MINOR_VERSION = '0';
+    const RELEASE_VERSION = '0';
+    const EXTRA_VERSION = 'alpha';
+
     public function registerBundles()
     {
         $bundles = [
@@ -16,6 +31,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -23,9 +43,39 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Oodle\KrumoBundle\OodleKrumoBundle();
         }
 
         return $bundles;
+    }
+
+    public function boot()
+    {
+        // Boot the Kernel
+        parent::boot();
+
+        // Set default locale for money_format() to USD
+        setlocale(LC_MONETARY, 'en_US');
+
+        // Set the default timezone
+        date_default_timezone_set('UTC');
+    }
+
+    public function getName()
+    {
+        return static::NAME;
+    }
+
+    public function getVersion()
+    {
+        return static::MAJOR_VERSION . '.' . static::MINOR_VERSION . '.' .
+        static::RELEASE_VERSION;
+    }
+
+    public function getFullVersion()
+    {
+        return static::MAJOR_VERSION . '.' . static::MINOR_VERSION . '.' .
+        static::RELEASE_VERSION . static::EXTRA_VERSION;
     }
 
     public function getRootDir()
