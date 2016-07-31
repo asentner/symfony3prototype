30c6395150de64cfdc35ea5f43618d573e04a4e8
Adam Sentner <asentner@coalmarch.com>
2016-07-31 09:14:30 -0400

    Install kint bundle and updating symfony

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -37,6 +37,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Oodle\KrumoBundle\OodleKrumoBundle();
+            $bundles[] = new Cg\KintBundle\CgKintBundle();
         }
 
         return $bundles;
